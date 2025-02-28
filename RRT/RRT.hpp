cb7aa089e65889dcdd8cad460bfea00e8937a77f
DW Wheeler <dawn.wheeler@gmail.com>
2025-02-27 21:13:21 -0800

    setting up files for RRT

---
+++ RRT/RRT.hpp
@@ -0,0 +1,13 @@
+#ifndef RRT_HPP
+#define RRT_HPP
+
+class RRT {
+public:
+  RRT();
+
+  void printSomething();
+
+
+};
+
+#endif
---
+++ RRT/runRRT.cpp
@@ -0,0 +1,12 @@
+//#include <opencv2/opencv.hpp>
+#include "RRT.hpp"
+
+// Compile with `clang++ -std=c++14 Astar.cpp -o Astar`
+
+using namespace std;
+
+int main() {
+  RRT rrt;
+  rrt.printSomething();
+  return 0;
+}
