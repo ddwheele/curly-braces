e95a7886f55b8b44670c6318c4f12e158cf6b146
DW Wheeler <dw.wheeler@nasa.gov>
2025-03-05 11:13:37 -0800

    Primitive tests pass

--- Planning/src/testUpdateableMinHeap.cpp
+++ Planning/src/testUpdateableMinHeap.cpp
@@ -4,13 +4,57 @@
 
 using namespace std;
 
-// clang++ -std=c++20 UpdateableMinHeap.cpp testUpdateableMinHeap.cpp -o testUpdateableMinHeap
+// clang++ -std=c++20 testUpdateableMinHeap.cpp -o testUpdateableMinHeap
+
+template <typename T>
+vector<T> extractMinHeap(UpdateableMinHeap<T> heap) {
+  vector<T> vec;
+  while(heap.getSize() > 0 ) {
+    vec.push_back(heap.extractMin());
+  }
+  return vec;
+}
+
+template <typename T>
+void printPrimitiveVector(vector<T> vec) {
+  for(int i=0; i<vec.size(); i++) {
+    cout << vec[i] << " ";
+  }
+  cout << endl;
+}
+
+template <typename T>
+bool destructiveIsSorted(UpdateableMinHeap<T> heap) {
+  T prev = heap.extractMin();
+  T next;
+  while(heap.getSize() > 0 ) {
+    next = heap.extractMin();
+    if(next < prev) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void testUpdateableMinHeapSimple() {
+  vector<int> ints = {4, 3, 2, 1};
+  UpdateableMinHeap<int> umh(ints);
+  // vector<int> fromHeap = extractMinHeap(umh);
+  // printPrimitiveVector(fromHeap);
+
+  // vector<int> sorted = {1, 2, 3, 4};
+
+  assert(destructiveIsSorted(umh));
+
+  cout << "  Passed testUpdateableMinHeapSimple" << endl;
+}
 
 void testUpdateableMinHeap() {
   vector<int> someInts = {67, 85, 37, 24, 90, 15, 24, 1975, 12, 43, 44, 3, 2, 1};
   UpdateableMinHeap<int> umh(someInts);
-  umh.printMe();
-
+  assert(destructiveIsSorted(umh));
+  // vector<int> fromHeap = extractMinHeap(umh);
+  // printPrimitiveVector(fromHeap);
 
   cout << "  Passed testUpdateableMinHeap" << endl;
 }
@@ -18,6 +62,7 @@ void testUpdateableMinHeap() {
 int main(int argc, char** argv) {
   cout << "Testing UpdateableMinHeap ..." << endl;
   testUpdateableMinHeap();
+  testUpdateableMinHeapSimple();
 
   cout << "All tests passed!" << endl;
   return 0;
